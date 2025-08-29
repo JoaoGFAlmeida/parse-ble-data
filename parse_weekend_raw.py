c1dcce51fee6515db7203f97a7756228ee01f4b9
Joao Almeida <jalmeida@performatti.com>
2025-08-29 11:10:01 -0300

    added weekend data

--- parse_weekend_raw.py
+++ parse_weekend_raw.py
@@ -3,6 +3,8 @@ import json
 from parse_func import get_acc_data
 
 df = pd.read_csv("weekend_data_22-24/raw_weekend.csv")
-
+df["acc"] = df.apply(lambda r: get_acc_data(r["rawData"]), axis=1)
 for i, row in df.iterrows():
-    print(f"{row['mac'][-3:]} - {row['timestamp']} - {get_acc_data(row['rawData'])}")
+    print(f"{row['mac'][-3:]} - {row['timestamp']} - {row['rawData']}//{row['acc']}")
+
+df.to_csv("weekend_data_22-24/acc_extracted_weekend.csv", index=False)
